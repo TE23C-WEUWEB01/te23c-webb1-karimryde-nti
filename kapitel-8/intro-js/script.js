6cbcae3f66e1f25cd44c028534302f1d52ccd099
Karim Ryde <karye.webb@gmail.com>
2025-03-13 14:27:28 +0100

    Tränat på Javascript, variabler, konkatenering

---
+++ kapitel-8/intro-js/script.js
@@ -0,0 +1,18 @@
+// var tal1 = 12;
+// var tal2 = 34;
+// var summa = tal1 + tal2;
+// alert(tal1 + " + " + tal2 + " = " + summa);
+
+var förnamn = "Karim";
+var efternamn = "Ryde";
+var ålder = 50;
+var namn = förnamn + " " + efternamn;
+//var meddelande = namn + " är " + ålder + " år gammal.";
+// alert(meddelande);
+
+var hobby = "segling";
+var kostnad = 18000;
+var meddelande = namn + " gillar " + hobby + "\noch det kostar " + kostnad + " per år."
+
+// Tex Karim Ryde gillar segling och det kostar ?? per år.
+alert(meddelande);
