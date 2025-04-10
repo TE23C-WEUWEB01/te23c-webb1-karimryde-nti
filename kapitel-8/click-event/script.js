39e37254183960b32016350b61ee2cec377f2a2b
Karim Ryde <karye.webb@gmail.com>
2025-04-10 14:32:36 +0200

    Tränat på click-event, ändra stil, och dölja/visa text o bild.

---
+++ kapitel-8/click-event/script.js
@@ -0,0 +1,15 @@
+function setDarkMode() {
+    document.body.style = "background: #000;color: #fff;";
+}
+function setLightMode() {
+    document.body.style = "background: #fff;color: #000;";
+}
+function setFunnyMode() {
+    document.querySelector("h1").style = "font-size:100px;";
+}
+function hide() {
+    document.querySelector("h1").style = "visibility: hidden;";
+}
+function show() {
+    document.querySelector("h1").style = "visibility: visible;";
+}
